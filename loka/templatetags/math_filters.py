1a73659adb77d78e5b5c3d7c125e6a56b6d6ac20
Kamal <10ejics727@gmail.com>
2025-01-30 19:15:46 +0530

    Update math_filters.py

--- loka/templatetags/math_filters.py
+++ loka/templatetags/math_filters.py
@@ -1,1 +1,11 @@
+from django import template
+
+register = template.Library()
+
+@register.filter
+def substract(value, arg):
+    try:
+        return int(value) - int(arg)
+    except (ValueError, TypeError):
+        return ''
 
