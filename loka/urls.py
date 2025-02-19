dffc25cc07371600c4d1f469ae242a290c635bca
Kamal <10ejics727@gmail.com>
2025-02-20 02:57:37 +0530

    Update urls.py

--- loka/urls.py
+++ loka/urls.py
@@ -7,8 +7,9 @@ urlpatterns = [
 
     path("", views.index, name="index"),
     path('add', views.SettlementAddView.as_view(), name="add_settlement"),
-    path('loka/', views.SettlementListView.as_view(), name="settlement_list"),
-    path('loka/deleteset/<int:id>', views.deleteset, name="deletesettlement"),
+    path('loka/', views.getsettlementlist, name="selectsettlements"),
+    path('loka/setlist/', views.SettlementListView.as_view(), name="settlement_list"),
+    path('loka/setlist/deleteset/<int:id>', views.deleteset, name="deletesettlement"),
     # path('gotohome/', views.SettlementListView.as_view(), name="gotohome"),
     path("siriusisadogstar/", views.register_request, name="register"),
     path("editprofile/", views.UserEditView.as_view(), name="editprofile"),
@@ -21,12 +22,8 @@ urlpatterns = [
     path("getladata1/", views.getladata1, name="getladata1"),
     path("getladata/", views.getladata, name="getladata"),
     path('loadregions/', views.load_regions, name='ajax_load_regions'),
-    path('loka/update/<int:pk>', views.SettlementUpdateView.as_view(), name='updaterec'),
-    path('change-password/', views.ChangePasswordView.as_view(), name='change_password'),
+    path('loka/setlist/update/<int:pk>', views.SettlementUpdateView.as_view(), name='updaterec'),
+    path('change-password/', ChangePasswordView.as_view(), name='change_password'),
     path('setcalc/', views.setcalc, name='setcalc'),
     path('generate-pdf/', views.generate_pdf, name='generate_pdf'),
-
-
-
-
 ]
